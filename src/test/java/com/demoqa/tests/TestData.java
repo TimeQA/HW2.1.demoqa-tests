b8e7bc0876d21fce1391f31153246ae74e2e6075
TimeQA <qaeducation2022@gmail.com>
2022-08-20 20:19:10 +0300

    added TestBase, TestData and changed TestPracticFormPageObjectWithFakerData

---
+++ src/test/java/com/demoqa/tests/TestData.java
@@ -0,0 +1,9 @@
+package com.demoqa.tests;
+
+public class TestData {
+    static String firstName = "Ivan",
+        lastName = "Navi",
+        email = "Ivan@navi.com",
+        number = "8999000881",
+        currentAddress = "currentAddress";
+}
---
+++ src/test/java/com/demoqa/tests/TestPracticeFormPageObjectWithFakerData.java
@@ -0,0 +1,83 @@
+package com.demoqa.tests;
+
+import com.demoqa.pages.TestPracticeFormPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.demoqa.tests.TestData.*;
+
+public class TestPracticeFormPageObjectWithFakerData extends TestBase{
+    TestPracticeFormPage testPracticFormPage = new TestPracticeFormPage();
+//
+//    String firstName,
+//            lastName,
+//            email,
+//            number,
+//            currentAddress;
+//
+//    @BeforeEach
+//    void prepareTestData() {
+//        firstName = "Ivan";
+//        lastName = "Navi";
+//        email = "Ivan@navi.com";
+//        number = "8999000881";
+//        currentAddress = "currentAddress";
+//    }
+
+    @Test
+    void practiceFormTest() {
+        testPracticFormPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setGenter("Female")
+                .setNumber(number)
+                .setBirthDate("5", "June", "1994")
+                .setSubjects("Physics")
+                .setHobbies("Sports")
+                .setPathFile("src/test/resources/JSON_Momoa.jpg")
+                .setAddress(currentAddress)
+                .setState("Haryana")
+                .setCity("Karnal");
+        $("#submit").pressEnter();
+
+
+        testPracticFormPage.checkResultVisible();
+        testPracticFormPage.checkResult("Student Name", "Ivan Navi")
+                .checkResult("Student Email", "Ivan@navi.com")
+                .checkResult("Gender", "Female")
+                .checkResult("Mobile", "8999000881")
+                .checkResult("Date of Birth", "05 June,1994")
+                .checkResult("Subjects", "Physics")
+                .checkResult("Hobbies", "Sports")
+                .checkResult("Picture", "JSON_Momoa.jpg")
+                .checkResult("Address", "currentAddress")
+                .checkResult("State and City", "Haryana Karnal");
+
+    }
+
+    @Test
+    void practiceFormTestMini() {
+        testPracticFormPage.openPage()
+                .setFirstName("Ivan")
+                .setLastName("Navi")
+                .setGenter("Female")
+                .setNumber("8999000881");
+
+        $("#dateOfBirthInput").click();
+        $("#submit").pressEnter();
+
+
+        $(".modal-header").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(
+                text("Ivan Navi"),
+                text("Female"),
+                text("8999000881"));
+    }
+}
+
+
+//        $(".table-responsive table").$(byText("Date of Birth"))
+//                .parent().shouldHave((text("05 June,1994")));
